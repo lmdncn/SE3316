efaeb1b7b14b427d8d4d2d6b895c42e0d0d4cde1
Liam Duncan <ldunca7@uwo.ca>
2016-11-10 23:13:47 +0000

    time to start p2

---
+++ Lab4/src/app/author.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthorService {
+
+    getAuthors():string[] {
+        
+        return ['Author1','Author2','Author3'];
+        
+    };
+
+  constructor() { }
+
+}
