29daeb587eb8dd9be0e10e748a300597f9ff84b0
ldunca7@uwo.ca <ldunca7@uwo.ca>
2016-11-20 13:53:47 -0500

    reseting new project in lab5 folder to file handout by prof

---
+++ Lab5/TabTastic/server/chord_api.js
@@ -0,0 +1,21 @@
+// Handling the chord_api
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.send('Got a GET request for the api');
+});
+
+router.put('/', function(req, res, next) {
+    res.send('Got a PUT request for the api');
+});
+
+router.post('/', function(req, res, next) {
+    res.send('Got a POST request for the api');
+});
+
+router.delete('/', function(req, res, next) {
+    res.send('Got a DELETE request for the api');
+});
+
+module.exports = router;
